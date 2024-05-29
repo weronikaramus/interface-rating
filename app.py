d9c0574a1e40ec38578ac921286cd3cd418451f0
Weronika <you@example.com>
2024-05-29 03:04:40 +0200

    update forms

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
-from flask import Flask, render_template, flash, session, redirect, url_for
+from flask import Flask, render_template, flash, session, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-import random
+from flask_migrate import Migrate
+from werkzeug.utils import secure_filename
+import os
+import uuid
+from flask_wtf.file import FileField
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///study.db'
-
+app.config['UPLOAD_FOLDER'] = 'static/img'  # Ensure this folder exists
 db = SQLAlchemy(app)
-
-
+migrate = Migrate(app, db)
 
 class SiteForm(FlaskForm):
-    name = StringField("Name: ", validators=[DataRequired()])
+    name = StringField("Nazwa: ", validators=[DataRequired()])
     url = StringField("URL: ", validators=[DataRequired()])
-    category = StringField("Category: ", validators=[DataRequired()])
-    type = StringField("Type: ", validators=[DataRequired()])
-    submit = SubmitField("Submit")
+    category = StringField("Kategoria: ", validators=[DataRequired()])
+    type = StringField("Typ: ", validators=[DataRequired()])
+    picture = FileField("Interfejs: ")  # Add this line
+    submit = SubmitField("DODAJ")
 
 class Site(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -27,14 +31,30 @@ class Site(db.Model):
     url = db.Column(db.String(200), nullable=False, unique=True)
     category = db.Column(db.String(200), nullable=False)
     type = db.Column(db.String(200), nullable=False)
+    picture = db.Column(db.String(200), nullable=True)
 
     def __repr__(self):
         return '<Name %r>' % self.name
 
+class QuestionForm(FlaskForm):
+    type = StringField("Type: ", validators=[DataRequired()])
+    q1 = StringField("1 question: ", validators=[DataRequired()])
+    q2 = StringField("2 question: ", validators=[DataRequired()])
+    q3 = StringField("3 question: ", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(200), nullable=False, unique=True)
+    q1 = db.Column(db.String(200), nullable=False)
+    q2 = db.Column(db.String(200), nullable=False)
+    q3 = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return '<Type %r>' % self.type
+
 with app.app_context():
     db.create_all()
-
-
 
 @app.route('/')
 def index():
@@ -43,10 +63,13 @@ def index():
 @app.route('/survey')
 def survey():
     our_sites = Site.query.all()
-    session['current_site'] = session.get('current_site', 0) + 1
-    current_site = our_sites[session['current_site']]
-    # current_site = random.choice(Site.query.all())
-    return render_template('survey.html', current_site=current_site)
+    session['current_site_index'] = session.get('current_site_index', -1) + 1
+    if session['current_site_index'] >= len(our_sites):
+        session['current_site_index'] = 0  # Reset to the beginning if we've gone past the last site
+
+    current_site = our_sites[session['current_site_index']]
+    question = Question.query.filter_by(type=current_site.type).first()
+    return render_template('survey.html', current_site=current_site, question=question)
 
 @app.route('/site/add', methods=['GET', 'POST'])
 def add_site():
@@ -54,28 +77,52 @@ def add_site():
     if form.validate_on_submit():
         site = Site.query.filter_by(url=form.url.data).first()
         if site is None:
-            site = Site(name=form.name.data, url=form.url.data, category=form.category.data, type=form.type.data)
+            pic_filename = None
+            if form.picture.data:
+                file = form.picture.data
+                pic_filename = secure_filename(file.filename)
+                pic_name = str(uuid.uuid1()) + "_" + pic_filename
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], pic_name))
+                pic_filename = pic_name
+
+            site = Site(
+                name=form.name.data,
+                url=form.url.data,
+                category=form.category.data,
+                type=form.type.data,
+                picture=pic_filename
+            )
             db.session.add(site)
             db.session.commit()
             flash("Site added to survey!")
         else:
             flash("Site already exists!")
-
         return redirect(url_for('add_site'))
 
     our_sites = Site.query.order_by(Site.id)
-
     return render_template("add_site.html", form=form, our_sites=our_sites)
 
-# Sample data
-@app.before_first_request
-def populate_db():
-    site1 = Site(name="TechCrunch", url="https://techcrunch.com", category="News", type="main")
-    site2 = Site(name="CNN", url="https://www.cnn.com", category="News", type="main")
+@app.route('/question/add', methods=['GET', 'POST'])
+def add_question():
+    form = QuestionForm()
+    if form.validate_on_submit():
+        question = Question.query.filter_by(type=form.type.data).first()
+        if question is None:
+            question = Question(type=form.type.data, q1=form.q1.data, q2=form.q2.data, q3=form.q3.data)
+            db.session.add(question)
+            db.session.commit()
+            flash("Questions added to survey!")
+        else:
+            flash("Questions already exists!")
 
-    db.session.add_all([site1, site2])
-    db.session.commit()
+        return redirect(url_for('add_question'))
 
+    our_questions = Question.query.order_by(Question.id)
+    return render_template("add_question.html", form=form, our_questions=our_questions)
+
+@app.route('/for_researchers', methods=['GET', 'POST'])
+def for_researchers():
+    return render_template("for_researchers.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
